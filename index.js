8d3cfc763b67c9ee654b6194aeda6ec5b35c416a
Oluwajoba <oluwajobabello@gmail.com>
2019-10-28 15:26:20 +0100

    set server to run on port 6000 / index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // implement your API here
+
+const express = require("express");
+
+const server = express();
+
+const port = 5000;
+
+server.listen(port, () => console.log(`running on port ${port}`))
