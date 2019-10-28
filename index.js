f3955eaaa18936d6871758ae538accbf8ba78d39
Oluwajoba <oluwajobabello@gmail.com>
2019-10-28 15:46:24 +0100

    make server send response when get/find is invoke / find() / index.js

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // implement your API here
 
 const express = require("express");
+const usersDB = require("./data/db")
 
 const server = express();
 
-const port = 6000;
+
 
 
 server.get("/", (req, res) => {
     res.send("hello world")
 })
+
+server.get("/users", (req, res) => {
+    usersDB
+    .find()
+    .then((user) => {
+        res.send(user)
+    })
+    .catch((err) => {
+        res.send(err)
+    })
+})
+
+
+const port = 6000;
 server.listen(port, () => console.log(`running on port ${port}`))
